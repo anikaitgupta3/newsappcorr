599a91774d9cb2fba924071634716aa4de8aa736
anikaitgupta3 <anikaitgupta3@gmail.com>
2023-09-24 22:27:26 +0530

    commit

--- app/src/main/java/com/example/newsapp/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/NewsViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.newsapp
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
-class NewsViewModel: ViewModel() {
+
+class NewsViewModel:ViewModel() {
 
     fun getNews(){
-        //viewModelScope
+        viewModelScope.launch()
     }//this method will be responsible for making the api call
 }
